dded174bbb94a696a5399806d86c0578ae049516
tkwhite <teresawhite021@gmail.com>
2023-10-24 19:43:37 -0500

    Q2

--- script/lahmans_baseball.sql
+++ script/lahmans_baseball.sql
@@ -18,20 +18,24 @@ FROM homegames;
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
 
 SELECT 
+	playerid,
 	namefirst || ' '|| namelast AS full_name,
 	height,
-	t.name AS team_name
+	t.name AS team_name,
+	(SELECT COUNT(g_all)
+		FROM appearances
+		WHERE playerid = 'gaedeed01') AS num_games_played
 FROM people p
-LEFT JOIN appearances a
+INNER JOIN appearances a
 	USING (playerid)
 LEFT JOIN teams t
 	USING (teamid)
-JOIN
-	(SELECT COUNT(g_all)
-	FROM appearances
-	WHERE p.playerid = shorty_id)
+LEFT JOIN fielding
+	USING (playerid)
 ORDER BY height
 LIMIT 1
+
+--a: Eddie Gaedel, 43", St Louis Browns, 1 game
 
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
