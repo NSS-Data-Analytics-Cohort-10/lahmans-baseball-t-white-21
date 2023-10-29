f7e5ea8e8dbf21c0498b60dc1961772182e44a95
tkwhite <teresawhite021@gmail.com>
2023-10-29 13:14:26 -0500

    Q10

--- script/lahmans_baseball.sql
+++ script/lahmans_baseball.sql
@@ -322,26 +322,26 @@ WHERE a.yearid = m.yearid
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
-player names - people
-max hr >=1 -batting?
-2016 
-league 10+yr - player (finalgame - debut need to cast as #)
+-- player names - people
+-- max hr >=1 -batting?
+-- 2016 
+-- league 10+yr - player (finalgame - debut need to cast as #)
 
 SELECT
 	namefirst || ' '|| namelast AS player,
-	max(hr)
+	max(hr) AS hr_high_2016
 FROM people
 LEFT JOIN batting
 	USING (playerid)
 INNER JOIN (SELECT playerid,
 			DATE_PART('year',finalgame::date)
-			DATE_PART('YEAR',debut::date) AS tenure
+			-DATE_PART('YEAR',debut::date) AS tenure
 			FROM people) AS s
 	USING(playerid)
 WHERE yearid = 2016
 	AND hr>=1
 	AND tenure >=10
-GROUP BY playeR
+GROUP BY player
 
 -- **Open-ended questions**
 
